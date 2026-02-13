1db16f64c5af42fdc072407cef1e8fd2ea3b1fb9
Umbra-devX <stealthdragonyi@gmail.com>
2026-02-13 15:44:57 +0100

    Update cards.js

--- LuxeGems-fashion-demo/src/data/cards.js
+++ LuxeGems-fashion-demo/src/data/cards.js
@@ -1,23 +1,24 @@
 export const cards = [
     {
         'name': 'Gold Hoop Earrings',
-        'img': 'LuxeGems-fashion-demo/src/assets/images/cardImg/earrings.png',
+        'img': '../assets/images/cardImg/earrings.png',
         'price': '5,000'
     },
     {
         'name': 'Elegant Necklace',
-        'img': 'LuxeGems-fashion-demo/src/assets/images/cardImg/necklace.png',
+        'img': '../assets/images/cardImg/necklace.png',
         'price': '6,200'
     },
     {
         'name': 'Pearl Bracelets',
-        'img': 'LuxeGems-fashion-demo/src/assets/images/cardImg/bracelet.png',
+        'img': '../assets/images/cardImg/bracelet.png',
         'price': '10,275'
     },
     {
         'name': 'Chick Statement Rings',
-        'img': 'LuxeGems-fashion-demo/src/assets/images/cardImg/ring.png',
+        'img': '../assets/images/cardImg/ring.png',
         'price': '9,900'
     }
 
 ]
+

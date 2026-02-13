b0db12add1090e6f191a25ff91aef3ecb9b04207
Umbra-devX <stealthdragonyi@gmail.com>
2026-02-13 15:48:45 +0100

    Update footer.jsx

--- LuxeGems-fashion-demo/src/components/Footer/footer.jsx
+++ LuxeGems-fashion-demo/src/components/Footer/footer.jsx
@@ -1,15 +1,17 @@
 import styles from './Footer.module.css'
-
+import instaLogo from '../../assets/images/instagram.png'
+import whatsappLogo from '../../assets/images/whatsapp.png'
 const Footer = ()=>{
     return(
         <footer className={styles.footer}>
             <span className={styles.footerComments}>2024 LuxeGems. All Rights Reserved</span>
             <span className={styles.socials}>
-                <a href=""><img src="/src/assets/images/instagram.png"/></a>
-                <a href=""><img src="/src/assets/images/whatsapp.png"/></a>
+                <a href=""><img src={instaLogo}/></a>
+                <a href=""><img src={whatsappLogo}/></a>
             </span>
         </footer>
     )
 }
+
 
 export default Footer
